35a441162e57f44414cc75990e8f2ca5597e2999
Gilang <gilangnurrizki173@gmail.com>
2024-10-20 17:11:02 +0700

    fix ui

--- app/src/main/java/com/dicoding/asclepius/view/MainActivity.kt
+++ app/src/main/java/com/dicoding/asclepius/view/MainActivity.kt
@@ -1,22 +1,13 @@
 package com.dicoding.asclepius.view
 
-import android.Manifest
-import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
-import androidx.activity.result.PickVisualMediaRequest
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.dicoding.asclepius.R
-import com.dicoding.asclepius.databinding.ActivityMainBinding
-import com.dicoding.asclepius.helper.ImageClassifierHelper
 import com.dicoding.asclepius.view.analyze.AnalyzeFragment
+import com.dicoding.asclepius.view.history.HistoryFragment
 import com.dicoding.asclepius.view.news.NewsFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import org.tensorflow.lite.task.vision.classifier.Classifications
 
 class MainActivity : AppCompatActivity() {
 
